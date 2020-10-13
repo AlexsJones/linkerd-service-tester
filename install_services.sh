5ca96913f085664dd283dd84ebe2f4a5d8691af6
Alex Jones <alexsimonjones@gmail.com>
2020-10-13 14:57:50 +0100

    fix(namespace): auto generate namespaces

--- install_services.sh
+++ install_services.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-kubectl create ns foo || true
-kubectl annotate ns foo linkerd.io/inject=enabled
-kubectl create ns bar || true
-kubectl annotate ns bar linkerd.io/inject=enabled
 helm install linkerd-service-tester .
